663dff49d4e5c5366a24e3b71f80453148df8d8e
MagedMohsen <maged.mohsen95@gmail.com>
2022-08-14 19:14:36 +0200

    On branch main

---
+++ problemSolving/leftRotation.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+    // declare a new array to store the rotated array
+    List<Integer> rot = new ArrayList<>();
+    for (int i =d;i<arr.size();i++){
+        //first, store the elements after the rotated elements
+        rot.add(arr.get(i));
+    }
+    for (int i =0;i<d;i++){
+        //then add the rotated elements
+        rot.add(arr.get(i));
+    }
+    //return the fully rotated array
+    return rot;
+    }
+
+}
+
+public class leftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ problemSolving/sparseArray.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+
+            //Note:  can be solved with HashMap but I prefer Arraylist
+
+    // create a new array for the count of the matching strings
+    List<Integer> matching = new ArrayList<>();
+    int count=0;
+    for (int i=0; i<queries.size();i++){ //for every string in the queries
+        for (int j =0;j<strings.size();j++){ //for every string in strings
+            //compare the queried string with the current string 
+            if (queries.get(i).equals(strings.get(j))){
+                //add your typical counter
+                count++;
+            }
+        }
+        //after matching a queried string with all strings, add the counter to the counter array
+        matching.add(count);
+        //reset the counter to use it again for the next query
+        count=0;
+        
+    }
+    return matching;
+
+    }
+
+}
+
+public class sparseArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = bufferedReader.readLine();
+            strings.add(stringsItem);
+        }
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = new ArrayList<>();
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = bufferedReader.readLine();
+            queries.add(queriesItem);
+        }
+
+        List<Integer> res = Result.matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.size(); i++) {
+            bufferedWriter.write(String.valueOf(res.get(i)));
+
+            if (i != res.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
